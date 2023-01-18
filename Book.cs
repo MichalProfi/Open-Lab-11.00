130d95ed97df3c8cb3def7848f6748c2815283e1
MartinHolena <martinkiko18@gmail.com>
2023-01-18 21:05:29 +0100

    done 11.00

---
+++ Book.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Lab_11._00
+{
+    class Book
+    {
+        public static List<string> categoryList = new List<string>() { "detské", "romantické", "náučné", "sci-fi", "dobrodružné" };
+
+        private int pages;
+        private string category;
+        private int releaseDate;
+        private string author;
+        private string title;
+
+        public string Author
+        {
+            get { return this.author; }
+            set { this.author = value; }
+        }
+
+        public string Category
+        {
+            set => category = value;
+        }
+        public int Pages
+        {
+
+            get => this.Pages;
+            set
+            {
+                if (value <= 1)
+                {
+                    this.pages = 1;
+                }
+                else
+                {
+                    this.pages = value;
+                }
+            }
+        }
+
+
+        public int ReleaseDate
+        {
+            get => this.releaseDate;
+            set
+            {
+                if (value < 1450 || value > 2020)
+                {
+                    releaseDate = -1;
+                }
+                else
+                {
+                    releaseDate = value;
+                }
+            }
+        }
+        public string Title
+        { get { return this.title; } set { this.title = value; } }
+
+
+        public Book()
+        {
+            title = "-1";
+            pages = -1;
+            category = "-1";
+            author = "-1";
+            releaseDate = -1;
+        }
+        public Book(string title, int pages)
+        {
+            this.title = title;
+            this.pages = pages;
+            category = "-1";
+            author = "-1";
+            releaseDate = -1;
+        }
+
+        public Book(string title, int pages, string category, string author, int releaseDate)
+        {
+            this.title = title;
+            this.pages = pages;
+            this.category = category;
+            this.author = author;
+            this.releaseDate = releaseDate;
+
+        }
+        public override string ToString()
+        {
+            return $"Title:{this.title}, Pages:{this.pages}, Category:{this.category}, Author:{this.author}, RealeaseDate:{this.releaseDate}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace Open_Lab_11._00
     {
         static void Main(string[] args)
         {
+            Library schooLibrary = new Library();
+            schooLibrary.studentsList.Add("Poul");
+            schooLibrary.booksList.Add(new Book("The Da Vinci Code", 608, "detective", "Dan Brown", 2003));
+            schooLibrary.booksList.Add(new Book("Harry Potter and the philosopher's stone", 640, "fantasy", "Joanne K. Rowling", 1997));
+
+            schooLibrary.studentsList.Add("John");
+            schooLibrary.booksList.Add(new Book("Labyrinth", 531, "historical fiction", "Kate Mosse", 2005));
+            schooLibrary.booksList.Add(new Book("Lost in the Jungle", 248, "biographcal", "Yossi Ghinsberg", 1985));
+
+            schooLibrary.All();
+            Console.ReadKey();
         }
     }
 }
